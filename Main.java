870959a7b1b2fa87c30a35d48098f1b9666f4010
Emre Cakar <emre.cakar@hs-hannover.de>
2023-05-10 16:34:51 +0200

    Main.java implementiert

---
+++ Main.java
@@ -0,0 +1,15 @@
+
+/**
+ * Klasse Main.
+ * 
+ * @author Prof. Dr.-Ing. Emre Çakar
+ * Labor - Angewandte Informatik
+ */
+
+public class Main
+{
+    public static void main (String args[])
+    {
+  
+    }
+}
